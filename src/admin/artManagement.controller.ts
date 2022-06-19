1db1086fbf0252d5df48414e3114ce490a428a9e
HaeChan Kim <bluesun147@naver.com>
2022-06-19 11:08:06 +0900

    [feat] swagger 도입

--- src/admin/artManagement.controller.ts
+++ src/admin/artManagement.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get, Param, Post, Delete, Patch, Body } from '@nestjs/common';
+import { ApiCreatedResponse, ApiOperation } from '@nestjs/swagger';
 import { FormDataRequest } from 'nestjs-form-data';
 import { ArtManagementService } from './artManagement.service';
 import { ArtManagementDto } from './dto/artManagement.dto';
@@ -10,12 +11,16 @@ export class ArtManagementController {
 
     // 작품 관리 메인 페이지 - 전체 작품 리스트 조회
     @Get('/arts')
+    @ApiOperation({ summary: '전체 작품 리스트 조회 API', description: '전체 작품 리스트 조회' }) // 요청 URL 에 매핑된 API 에 대한 설명
+    @ApiCreatedResponse({ description: '전체 작품 리스트 조회', type: Art }) // API 응답에 대한 정의
     getList(): Promise <Art[]> {
         return this.artManagementService.getArts();
     }
 
     // 작품 업로드 페이지
     @Post('/arts/upload')
+    @ApiOperation({ summary: '작품 업로드 API', description: '작품 업로드' })
+    @ApiCreatedResponse({ description: '작품 업로드', type: Art })
     @FormDataRequest()
     uploadArt(@Body() artManagementDto: ArtManagementDto): Promise<Art> {
         return this.artManagementService.uploadArt(artManagementDto);
@@ -23,18 +28,24 @@ export class ArtManagementController {
 
     // 작품 상세 페이지
     @Get('/arts/:id')
+    @ApiOperation({ summary: '개별 작품 조회 API', description: '개별 작품 조회' })
+    @ApiCreatedResponse({ description: '개별 작품 조회', type: Art })
     getArtById(@Param('id') id: number): Promise<Art> {
         return this.artManagementService.getArtById(id);
     }
 
     // 작품 수정
     @Patch('/arts/:id')
+    @ApiOperation({ summary: '개별 작품 수정 API', description: '개별 작품 수정' })
+    @ApiCreatedResponse({ description: '개별 작품 수정', type: Art })
     updateFile(@Param('id') id: number): string {
         return this.artManagementService.updateArt(id);
     }
 
     // 작품 삭제
     @Delete('/arts/:id')
+    @ApiOperation({ summary: '개별 작품 삭제 API', description: '개별 작품 삭제' })
+    @ApiCreatedResponse({ description: '개별 작품 삭제', type: Art })
     deleteFile(@Param('id') id: number): string {
         return this.artManagementService.deleteArt(id);
     }
