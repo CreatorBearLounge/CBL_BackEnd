882cdaa7239aad46b76970b6c98de76a70f3835a
HaeChan Kim <bluesun147@naver.com>
2022-07-05 21:34:26 +0900

    [Add] heroku 배포 관련 설정

--- src/main.ts
+++ src/main.ts
@@ -23,6 +23,7 @@ async function bootstrap() {
     const document = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup("docs", app, document);
 
-    await app.listen(3000);
+    app.enableCors();
+    await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
