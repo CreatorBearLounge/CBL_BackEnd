cf43c2143071da74980926734d0ebc2bf26271e3
HaeChan Kim <bluesun147@naver.com>
2022-08-07 13:18:01 +0900

    [chore] config 수정

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -5,11 +5,10 @@ export const typeORMConfig : TypeOrmModuleOptions = {
     type: 'postgres',
     host: process.env.DB_HOST,
     // host: 'cbl.csnv6qgbbdm9.ap-northeast-2.rds.amazonaws.com',
-    port: parseInt(process.env.PORT),
+    port: 5432,
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     entities: [__dirname + '/../**/*.entity.{js, ts}', __dirname + '/../**/*.repository.{js, ts}'],
     synchronize: true,
-    // logging : true,
 }
