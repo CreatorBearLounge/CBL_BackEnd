07af236d6b0dc4fba341ef5a625d6af02fe14a0e
HaeChan Kim <bluesun147@naver.com>
2022-08-07 13:36:36 +0900

    [chore] config entities 수정

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -8,6 +8,6 @@ export const typeORMConfig : TypeOrmModuleOptions = {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
-    entities: [__dirname + '/../**/*.entity.{js, ts}', __dirname + '/../**/*.repository.{js, ts}'],
+    entities: [__dirname + '/../**/*.entity.{js, ts}'], //entities: [__dirname + '/../**/*.entity.{js, ts}', __dirname + '/../**/*.repository.{js, ts}'],
     synchronize: true,
 }
